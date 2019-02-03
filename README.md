78380fe5d45f7454e3926b89128996249f468db3
turner-kevin <turner.kevin@gmail.com>
2019-02-03 13:44:46 -0800

    add .gitignore

---
+++ README.md
@@ -0,0 +1,1 @@
+# Identifying-Fraud-at-Enron
---
+++ poi_id.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+
+import sys
+import pickle
+sys.path.append("../tools/")
+
+from feature_format import featureFormat, targetFeatureSplit
+from tester import dump_classifier_and_data
+
+### Task 1: Select what features you'll use.
+### features_list is a list of strings, each of which is a feature name.
+### The first feature must be "poi".
+features_list = ['poi',
+                 'bonus',
+                 'exercised_stock_options',
+                 'salary',
+                 'total_stock_value'] 
+                 # bouns_prct will be added when the feature is created
+
+### Load the dictionary containing the dataset
+with open("final_project_dataset.pkl", "r") as data_file:
+    data_dict = pickle.load(data_file)
+
+### Task 2: Remove outliers
+# remove 'total' record that contains totals for each feature    
+data_dict.pop('TOTAL',0)
+
+### Task 3: Create new feature(s)
+# create a feature that captures percent of bonus to salary
+def percent_bonus_to_salary(data_dict, features_list):
+    
+    for record in data_dict:
+        person = data_dict[record]
+                
+        if person['salary'] == 'NaN'or person['bonus'] == 'NaN':
+            person['bonus_prct'] = 'NaN'
+        else:
+            person['bonus_prct'] = \
+            float(person['bonus']) / float(person['salary'])
+
+    features_list += ['bonus_prct']
+
+### Store to my_dataset for easy export below.
+my_dataset = data_dict
+
+### Extract features and labels from dataset for local testing
+data = featureFormat(my_dataset, features_list, sort_keys = True)
+labels, features = targetFeatureSplit(data)
+
+### Task 4: Try a varity of classifiers
+### Please name your classifier clf for easy export below.
+### Note that if you want to do PCA or other multi-stage operations,
+### you'll need to use Pipelines. For more info:
+### http://scikit-learn.org/stable/modules/pipeline.html
+
+# Provided to give you a starting point. Try a variety of classifiers.
+
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+
+# teseted several algorithms
+#from sklearn.tree import DecisionTreeClassifier
+#clf = DecisionTreeClassifier()
+#from sklearn.neighbors import KNeighborsClassifier
+#clf = KNeighborsClassifier()
+#from sklearn.ensemble import RandomForestClassifier
+#clf = RandomForestClassifier()
+
+
+### Task 5: Tune your classifier to achieve better than .3 precision and recall 
+### using our testing script. Check the tester.py script in the final project
+### folder for details on the evaluation method, especially the test_classifier
+### function. Because of the small size of the dataset, the script uses
+### stratified shuffle split cross validation. For more info: 
+### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
+
+# Example starting point. Try investigating other evaluation techniques!
+from sklearn.model_selection import train_test_split
+
+features_train, features_test, labels_train, labels_test = \
+    train_test_split(features, labels, test_size=0.3, random_state=42)
+
+### Task 6: Dump your classifier, dataset, and features_list so anyone can
+### check your results. You do not need to change anything below, but make sure
+### that the version of poi_id.py that you submit can be run on its own and
+### generates the necessary .pkl files for validating your results.
+
+dump_classifier_and_data(clf, my_dataset, features_list)
